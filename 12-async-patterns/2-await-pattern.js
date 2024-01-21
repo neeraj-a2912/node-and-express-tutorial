f5327de80733a4199b0cd64eb99e57e920e3a49e
Neeraj Aloney <neeraj.aloney2001@gmail.com>
2024-01-21 11:40:24 +0530

    Async patterns added

---
+++ 12-async-patterns/2-await-pattern.js
@@ -0,0 +1,44 @@
+const { readFile, writeFile } = require("fs").promises;
+
+const start = async () => {
+  try {
+    const first = await readFile("../content/first.txt");
+    const second = await readFile("../content/second.txt");
+    await writeFile(
+      "../content/result-mind-grenade.txt",
+      `THIS IS AWESOME : ${first} ${second}`,
+      { flag: "a" }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+
+
+// readFile("../content/first.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     return;
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf-8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("../content/first.txt")
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err));
+
