789fc3db6b1645a9945b11796889603ed2d468e3
Neeraj Aloney <neeraj.aloney2001@gmail.com>
2024-01-25 21:22:12 +0530

    Basic Server Created

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -1,1 +1,10 @@
-console.log("express files") 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("hi user");
+  console.log("user hit the server");
+});
+
+server.listen(5000, () => {
+  console.log("Server Running at port 5000");
+});
